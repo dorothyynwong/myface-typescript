a199ebad6167c1cd0457b9568aaef40c5f1fb907
Dorothy Wong <dorothy.wong@softwire.com>
2024-07-18 13:32:25 +0100

    make menu appear and disappear

--- public/js/app.js
+++ public/js/app.js
@@ -4,11 +4,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function changeBackground() {
   // Add your JavaScript here.
+  
   document.body.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
 };
 
 function displayMenu() {
   let menuPad = document.getElementById("menuPad");
-  console.log(menuPad);
-  menuPad.setAttribute("display", "flex");
+  const displayValue = menuPad.className;
+  if (displayValue === "menuPadHidden")  menuPad.setAttribute("class","menuPadShow");
+  else  menuPad.setAttribute("class","menuPadHidden");
 }
