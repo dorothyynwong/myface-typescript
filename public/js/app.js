5c2f7d975207bc31fffb74da7feb0076d3f0c601
Dorothy Wong <dorothy.wong@softwire.com>
2024-07-18 11:35:41 +0100

    1. add button to change background randomly 2. add menu to post_list 3. add button to display menu (not work yet)

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Add your JavaScript here.
 });
+
+function changeBackground() {
+  // Add your JavaScript here.
+  document.body.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+};
+
+function displayMenu() {
+  let menuPad = document.getElementById("menuPad");
+  console.log(menuPad);
+  menuPad.setAttribute("display", "flex");
+}
